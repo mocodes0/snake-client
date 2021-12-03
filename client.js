4d057ba09213428fdb4af62d0ad1806a54b42c7a
Mohamed Bashir <mo.dev416@gmail.com>
2021-12-03 05:43:32 +0000

    clarified constants plus client test

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const { IP, PORT } = require('./constants');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+  //Interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server. Thank you for your patience.");
+    conn.write('Name: MOE');
+    //setInterval(() => {
+    
+  });
+  return conn;
+};
+
+module.exports = connect;
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = '76.69.53.72';
+const PORT = 50541;
+
+module.exports = {
+  IP,
+  PORT
+};
